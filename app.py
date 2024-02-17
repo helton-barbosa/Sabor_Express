96f402442db171e602c5d33cd3647b2b40ca7c92
Helton Barbosa <heltonbs@gmail.com>
2024-02-17 17:15:31 -0300

    Alterando a fontes do Menu principal

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-print('Sabor Express\n')
+print('＞ Ｓａｂｏｒ Ｅｘｐｒｅｓｓ ＜\n')
 
-print('1. Cadastrar Restaurante')
-print('2. Listar Restaurante')
-print('3. Ativar Restaurante')
-print('4. Sair\n')
+print('1. C̳a̳d̳a̳s̳t̳r̳a̳r̳ R̳e̳s̳t̳a̳u̳r̳a̳n̳t̳e̳')
+print('2. L̳i̳s̳t̳a̳r̳ R̳e̳s̳t̳a̳u̳r̳a̳n̳t̳e̳')
+print('3. A̳t̳i̳v̳a̳r̳ R̳e̳s̳t̳a̳u̳r̳a̳n̳t̳e̳')
+print('4. S̳a̳i̳r̳\n')
 
 opcao_escolhida = input('Escolha uma opção: ')
 print(f'Você escolheu a opção {opcao_escolhida}')
